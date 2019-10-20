f72539ee209a4e0481a1e201025ba10e2a4efe85
Jonas Hochsteger <jonas.hochsteger.04@gmail.com>
2019-10-20 15:01:37 +0200

    fixed the issue, that the window closes even if you say not to

--- src/main/java/net/battleships/game/Window.java
+++ src/main/java/net/battleships/game/Window.java
@@ -42,13 +42,14 @@ public class Window {
     private void startup(){
         //New Frame
         this.frame = new JFrame(this.name);
+        frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
         frame.setVisible(true);
         frame.setSize(width, height);
-        //closes the window if you press on the X
+        //closes the window if you press on the X, but with a confirmation Dialog, to confirm that the user wants to close it
         frame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent we) {
-                if(JOptionPane.showConfirmDialog(frame, "Are you sure you want to quit?", "Are you sure you want to quit?", JOptionPane.YES_NO_OPTION)==0){
+                if(JOptionPane.showConfirmDialog(frame, "If you quit you will lose the game", "Are you sure you want to quit?", JOptionPane.YES_NO_OPTION)==0){
                     frame.dispose();
                 }
             }
