c9e903c81939fe2cf79bb79e105892c9c59c6c85
Jonas Hochsteger <jonas.hochsteger.04@gmail.com>
2019-10-20 11:16:56 +0200

    Made a working window with java awt

---
+++ src/main/java/net/battleships/game/Test.java
@@ -0,0 +1,7 @@
+package net.battleships.game;
+
+public class Test {
+    public static void main(String[] args) {
+        Window window = new Window("Battleships", 500, 500);
+    }
+}
---
+++ src/main/java/net/battleships/game/Window.java
@@ -0,0 +1,36 @@
+package net.battleships.game;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class Window {
+    int width;
+    int heigth;
+    String name;
+
+    public Window(String name) {
+        this.width = 100;
+        this.heigth = 100;
+        this.name = name;
+        this.startup();
+    }
+
+    public Window(String name, int width, int heigth) {
+        this.width = width;
+        this.heigth = heigth;
+        this.name = name;
+        this.startup();
+    }
+
+    private void startup(){
+        Frame f = new Frame(this.name);
+        f.setVisible(true);
+        f.setSize(width, heigth);
+        f.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent we) {
+                f.dispose();
+            }
+        } );
+    }
+}
