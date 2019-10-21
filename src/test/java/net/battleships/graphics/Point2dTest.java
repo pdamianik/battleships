bd76bb41ed9b930ccda0aebfe0d250945a570b0f
Jonas Hochsteger <jonas.hochsteger.04@gmail.com>
2019-10-21 21:26:35 +0200

    changed language to english instead of german and fixed errors

--- src/test/java/net/battleships/graphics/Point2dTest.java
+++ src/test/java/net/battleships/graphics/Point2dTest.java
@@ -8,16 +8,16 @@ public class Point2dTest {
     @Test
     public void testDistanceNull() {
         Point2D point2D = new Point2D(2, 2);
-        assertEquals(new Point2D(2, 2).getX(), point2D.abstandNull().getX());
-        assertEquals(new Point2D(2, 2).getY(), point2D.abstandNull().getY());
+        assertEquals(new Point2D(2, 2).getX(), point2D.distanceNull().getX());
+        assertEquals(new Point2D(2, 2).getY(), point2D.distanceNull().getY());
     }
 
     @Test
     public void testDistance() {
         Point2D point1 = new Point2D(2, 2);
         Point2D point2 = new Point2D(4, 4);
-        assertEquals(2, point1.abstand(point2).getX());
-        assertEquals(2, point1.abstand(point2).getY());
+        assertEquals(2, point1.distance(point2).getX());
+        assertEquals(2, point1.distance(point2).getY());
     }
 
     @Test
