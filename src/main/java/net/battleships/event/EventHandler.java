8738d4ffcbe784909da4827312ad9c9859f6f404
rasphi <pdamianik@student.tgm.ac.at>
2019-11-05 13:19:34 +0100

    Finished Event Handler

--- src/main/java/net/battleships/event/EventHandler.java
+++ src/main/java/net/battleships/event/EventHandler.java
@@ -4,14 +4,30 @@ import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Represents an event handler object to handle events (implements interface Event) with handlers (implements interface Handler)
+ * @author Philip Damianik
+ * @version 2019-11-05
+ */
+
 public class EventHandler {
-	private HashMap<Event, HashMap<EventPriority, ArrayList<Handler>>> handlers;
+	private HashMap<Event, HashMap<EventPriority, ArrayList<Handler>>> handlers; // the handlers
+
+	/**
+	 * A constructor without magic; simply initializes the handlers hashmap
+	 */
 
 	public EventHandler() {
 		this.handlers = new HashMap<>();
 	}
 
-	public void register(Handler handler, EventPriority priority) throws InvalidParameterException {
+	/**
+	 * Registers a handler to handle an event with a priority
+	 * @param handler the handler that handles the event
+	 * @param priority the priority for the handler (lower means later executed)
+	 */
+
+	public void register(Handler handler, EventPriority priority) {
 		if (this.handlers.containsKey(handler.getEvent())) {
 			HashMap<EventPriority, ArrayList<Handler>> priorityHandler = this.handlers.get(handler.getEvent());
 			if (priorityHandler.containsKey(priority)) {
@@ -30,28 +46,32 @@ public class EventHandler {
 		}
 	}
 
+	/**
+	 * Registers a handler for an event with medium priority (default)
+	 * @param handler the event handler to handle the event
+	 */
+
 	public void register(Handler handler) {
 		this.register(handler, EventPriority.MEDIUM);
 	}
 
+	/**
+	 * Handles an event
+	 * @param event the event to handle
+	 */
+
 	public void handle(Event event) {
 		if (this.handlers.containsKey(event)) {
 			HashMap<EventPriority, ArrayList<Handler>> handlersByPriority = this.handlers.get(event);
-			if (handlersByPriority.containsKey(EventPriority.HIGH)) {
-				for (Handler handler : handlersByPriority.get(EventPriority.HIGH)) {
+			if (handlersByPriority.containsKey(EventPriority.HIGH))
+				for (Handler handler : handlersByPriority.get(EventPriority.HIGH))
 					handler.handle(event);
-				}
-			}
-			if (handlersByPriority.containsKey(EventPriority.MEDIUM)) {
-				for (Handler handler : handlersByPriority.get(EventPriority.MEDIUM)) {
+			if (handlersByPriority.containsKey(EventPriority.MEDIUM))
+				for (Handler handler : handlersByPriority.get(EventPriority.MEDIUM))
 					handler.handle(event);
-				}
-			}
-			if (handlersByPriority.containsKey(EventPriority.LOW)) {
-				for (Handler handler : handlersByPriority.get(EventPriority.LOW)) {
+			if (handlersByPriority.containsKey(EventPriority.LOW))
+				for (Handler handler : handlersByPriority.get(EventPriority.LOW))
 					handler.handle(event);
-				}
-			}
 		}
 	}
 }
